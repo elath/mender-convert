527832b7b3dc7966163ad5e1a0355c422de085f2
Bryce Klippenstein <bryce.klippenstein@gmail.com>
2024-10-14 10:19:17 -0600

    Interm commit

--- rpi-prep/rpi-prep.sh
+++ rpi-prep/rpi-prep.sh
@@ -38,35 +38,38 @@ if ! test -d ./boot; then
     mkdir boot
 fi
 
-cp $base_img $tmp_img
+if ! test -f ./$tmp_img; then
+    echo "Creating tmp.img"
+    install -m 660 $base_img $tmp_img
+fi
 
-# Resize the rootfs partition of the temp image to accomodate the
-# updates and installed applications.
-# This is written for an image where the rootfs is partition #2
-qemu-img resize -f raw $tmp_img $tmp_img_size
-growpart $tmp_img 2
-
-loop_device=$(sudo losetup -f --show -P $tmp_img)
-# echo $loop_device
-sudo e2fsck -f -y ${loop_device}p2
-sudo resize2fs ${loop_device}p2
-
-# Mount our file systems, and support for local hardware
-
-sudo mount ${loop_device}p1 ./boot
-sudo mount ${loop_device}p2 ./rootfs
-
-sudo mount -t proc /proc ./rootfs/proc
-sudo mount --bind /dev ./rootfs/dev
-sudo mount --bind /sys ./rootfs/sys
-sudo mount --bind /run ./rootfs/run
-
-echo "Install setup script into rootfs."
-sudo install -o root -m 744 rpi-chroot-script_part1.sh rootfs/root/
-sudo install -o root -m 744 rpi-chroot-script_part2.sh rootfs/root/
-
-eval $(ssh-agent -s)
 if test ! -f ~/$prepared_img_part1; then
+
+    # Resize the rootfs partition of the temp image to accomodate the
+    # updates and installed applications.
+    # This is written for an image where the rootfs is partition #2
+    qemu-img resize -f raw $tmp_img $tmp_img_size
+    growpart $tmp_img 2
+
+    loop_device=$(sudo losetup -f --show -P $tmp_img)
+    # echo $loop_device
+    sudo e2fsck -f -y ${loop_device}p2
+    sudo resize2fs ${loop_device}p2
+
+    # Mount our file systems, and support for local hardware
+
+    sudo mount ${loop_device}p1 ./boot
+    sudo mount ${loop_device}p2 ./rootfs
+
+    sudo mount -t proc /proc ./rootfs/proc
+    sudo mount --bind /dev ./rootfs/dev
+    sudo mount --bind /sys ./rootfs/sys
+    sudo mount --bind /run ./rootfs/run
+
+    echo "Install setup script into rootfs."
+    sudo install -o root -m 744 rpi-chroot-script_part1.sh rootfs/root/
+    sudo install -o root -m 744 rpi-chroot-script_part2.sh rootfs/root/
+
 	#Enter into image
     sudo chroot rootfs bash -c '/root/rpi-chroot-script_part1.sh'
 
@@ -80,6 +83,14 @@ if test ! -f ~/$prepared_img_part1; then
 
     cp $tmp_img $prepared_img_part1
 fi
+
+sudo mount ${loop_device}p1 ./boot
+sudo mount ${loop_device}p2 ./rootfs
+
+sudo mount -t proc /proc ./rootfs/proc
+sudo mount --bind /dev ./rootfs/dev
+sudo mount --bind /sys ./rootfs/sys
+sudo mount --bind /run ./rootfs/run
 
 #Enter into image
 sudo chroot rootfs bash -c '/root/rpi-chroot-script_part2.sh'
@@ -98,4 +109,4 @@ sudo umount rootfs/run
 sudo umount rootfs
 sudo losetup -d ${loop_device}*
 
-cp $tmp_img $prepared_img
+install -m 440 $tmp_img $prepared_img
